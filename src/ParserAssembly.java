9005da1caa1b0c11b6d32dd56376a07073ab56e7
PAWEENNUTpuyfai <paweennut.pp@outlook.com>
2024-09-19 00:22:45 +0700

    Initial parser

---
+++ src/ParserAssembly.java
@@ -0,0 +1,115 @@
+import java.util.ArrayList;
+import java.util.List;
+public class ParserAssembly {
+    private Token tkz;
+    private static final List<String> AssemblyCommand = CreateAssemblyCommand();
+    private ArrayList<String> machineCoode = new ArrayList<>();
+    private String currentCode ;
+    public ParserAssembly(Token tkz){
+        this.tkz = tkz;
+        this.currentCode = "";
+    }
+
+    public void parseCode() throws Exception{
+        if(tkz.hasNextToken()){
+            this.parseInstruction();
+        }
+    }
+
+    public void parseInstruction() throws Exception{
+        if(AssemblyCommand.contains(tkz.peek())){
+            parseCommand();
+        }else{
+            parseLable();
+        }
+    }
+
+    public void parseCommand() throws Exception{
+        if(tkz.peek().equals("add")){
+            currentCode = "000";
+            parseReg();
+        }else if(tkz.peek().equals("nand")){
+            currentCode = "001";
+            parseReg();
+        }else if(tkz.peek().equals("lw")){
+            currentCode = "010";
+            parseReg();
+        }else if(tkz.peek().equals("sw")){
+            currentCode = "011";
+            parseReg();
+        }else if(tkz.peek().equals("beq")){
+            currentCode = "100";
+            parseReg();
+        }else if(tkz.peek().equals("jalr")){
+            currentCode = "101";
+            parseReg();
+        }else if(tkz.peek().equals("halt")){
+            currentCode = "110";
+            parseInstruction();
+        }else if(tkz.peek().equals("noop")){
+            currentCode = "111";
+            parseInstruction();
+        }
+    }
+
+    public void parseLable() throws Exception{
+
+    }
+
+    public void parseFill() throws Exception{
+
+    }
+    public void parseReg() throws Exception{
+        if(isNumber(tkz.peek())){
+            Integer reg = Integer.parseInt(tkz.peek());
+            if(reg < 7){
+                byte b = reg.byteValue();
+                String s = new String(String.valueOf(b));
+                currentCode = currentCode + s;
+            }
+        }
+    }
+
+    public void parseImm() throws Exception{
+
+    }
+
+    public void parseGapRType() throws Exception{
+
+    }
+
+    public void parseGapJType() throws Exception{
+
+    }
+
+    public void parseGapOType() throws Exception{
+
+    }
+
+    public void parseGap31_25()throws Exception{
+
+    }
+
+    private boolean isNumber(String str) {
+        //check string is number
+        int size = str.length();
+        for (int i = 0; i < size; i++) {
+            if (!Character.isDigit(str.charAt(i))) {
+                return false;
+            }
+        }
+        return size > 0;
+    }
+    private static List<String> CreateAssemblyCommand(){
+        List<String> list = new ArrayList<>();
+        list.add("add");
+        list.add("nand");
+        list.add("lw");
+        list.add("sw");
+        list.add("beq");
+        list.add("jalr");
+        list.add("halt");
+        list.add("noop");
+        return list;
+    }
+}
