234eecd7f3c6ac35b98d6d71b725372ba01d20cc
PAWEENNUTpuyfai <paweennut.pp@outlook.com>
2024-09-21 11:29:43 +0700

    non test

--- src/Main2.java
+++ src/Main2.java
@@ -1,6 +1,6 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
+public class Main2 {
     public static void main(String[] args) throws Exception {
         // Press Opt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
