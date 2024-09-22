f25424d20f7e0d89c1e246cb071d6e0f3193b4ab
Issawara <kmind46@gmail.com>
2024-09-22 10:43:49 +0700

    ๊

--- src/Main.java
+++ src/Main.java
@@ -6,38 +6,11 @@ import java.util.ArrayList;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
-        ArrayList<String> memory = new ArrayList<>();
-        memory.add("0100000010000000000000111");
-        memory.add("0100010100000000000000011");
-        memory.add("0000010100000000000000001");
-        memory.add("1000000010000000000000010");
-        memory.add("1000000001111111111111101");
-        memory.add("1110000000000000000000000");
-        memory.add("1100000000000000000000000");
-        memory.add("0000000000000101");
-        memory.add("1111111111111111");
-        memory.add("0000000000000010");
-        BSimulator sim = new BehavioralSimulator(memory);
-        sim.run();
-
-//        String bi = Integer.toBinaryString(65535);
-//        String bi2 = Integer.toBinaryString(65535 & (1<<15));
-//        System.out.println(bi+"\n"+bi2);
-//        System.out.println(BehavioralSimulator.convertNum(65535));
-
-//        String bi = Integer.toBinaryString(-3).substring(16);
-//        System.out.println(bi);
+    public static void main(String[] args) throws Exception {
+        ReadAssembly RA = new ReadFile("src\\tonnamTest.txt");
+        ArrayList<String> MC = RA.getMachineCode();
+//        MC.set(3,"0001110010000000000000111");
+//        BSimulator BS = new BehavioralSimulator(MC);
+//        BS.run();
     }
 }
