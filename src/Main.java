958c2c04fbc8718e7a4994d29a35098ebf6199b4
Ta0huu <Anuwatch6552@gmail.com>
2024-09-24 11:38:52 +0700

    rename Multiply

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) throws Exception {
 //        ReadAssembly RA = new ReadFile("src\\test2.txt");
-        ReadAssembly RA = new ReadFile("src\\tonnamTest.txt");
+        ReadAssembly RA = new ReadFile("src\\Multiply.txt");
 //        ReadAssembly RA = new ReadFile("src\\example.txt");
         ArrayList<String> MC = RA.getMachineCode();
         BSimulator BS = new BehavioralSimulator(MC);
