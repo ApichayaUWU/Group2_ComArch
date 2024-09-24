004318c7936f79a62f4a82358b1297ba01f7bffe
Issawara <kmind46@gmail.com>
2024-09-24 16:47:39 +0700

    MainFix

--- src/Main.java
+++ src/Main.java
@@ -9,10 +9,9 @@ public class Main {
     public static void main(String[] args) throws Exception {
         ReadAssembly RA = new ReadFile("src\\Combination.txt");
 //        ReadAssembly RA = new ReadFile("src\\Multiply.txt");
-//        ReadAssembly RA = new ReadFile("src\\example.txt");
         ArrayList<String> MC = RA.getMachineCode();
-//        BSimulator BS = new BehavioralSimulator(MC,"src\\MultiplyOutput.txt");
         BSimulator BS = new BehavioralSimulator(MC,"src\\CombinationOutput.txt");
+//        BSimulator BS = new BehavioralSimulator(MC,"src\\MultiplyOutput.txt");
         BS.run();
     }
 }
