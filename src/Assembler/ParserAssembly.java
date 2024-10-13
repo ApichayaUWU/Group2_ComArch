afb1d4c99e93983063ed9badff7b00ca7b3b8580
PAWEENNUTpuyfai <paweennut.pp@outlook.com>
2024-10-13 12:46:25 +0700

    comment

--- src/Assembler/ParserAssembly.java
+++ src/Assembler/ParserAssembly.java
@@ -13,63 +13,68 @@ public class ParserAssembly {
     private ArrayList<Integer> address = new ArrayList<Integer>();
 
     public ParserAssembly(Tokenizer tkz) throws Exception {
+        // receive Tokenizer tkz and initial currentCode for assign machine code to machineCode
         this.tkz = tkz;
         this.currentCode = "";
+        //start read token and convert assembly language to machine code
         this.parseCode();
     }
 
     public ArrayList<String> getMachineCode(){
+        //send ArrayList<String> machineCode that contain code in file
         return machineCode;
     }
     public void parseCode() throws Exception{
+        //read token if file not empty
         if(tkz.hasNextToken()){
             this.parseInstruction();
         }
     }
 
     public void parseInstruction() throws Exception{
-        int i=0;
+        //read token until last token
         while(tkz.hasNextToken()) {
             if (AssemblyCommand.contains(tkz.peek())) {
-//                System.out.println("command   " + tkz.peek() +"  " + i++);
+                //if token is command in instruction name in assembly language
                 parseCommand();
             }else {
-//                System.out.println("label" + tkz.peek());
+                //if token is not command assume this token is label
                 parseLabel();
             }
         }
+        //when last token fill label with address follow syntax of assembly language
         fillLabel();
     }
 
     public void parseCommand() throws Exception{
+        //add opcode to bit 22-24 in currentCode of each name of instruction
         if(tkz.peek().equals("add")){
             currentCode = "000";
             tkz.consume();
             for(int i=0; i<3; i++){
-//                System.out.println("add   " + i +tkz.peek());
+                //add regA regB destReg follow assembly language
                 parseReg();
             }
+            //add bit 3-15 with 0
             parseGapRType();
-//            System.out.println("your here ?" + tkz.peek());
+            //ignore token After this, until the end of the line
             parseComment();
-//            if(tkz.peek().equals("!")) {
-//
-//            }
         }else if(tkz.peek().equals("nand")){
             currentCode = "001";
             tkz.consume();
             for(int i=0; i<3; i++){
-//                System.out.println("nand   " + i +tkz.peek());
+                //add regA regB destReg follow assembly language
                 parseReg();
             }
+            //add bit 3-15 with 0
             parseGapRType();
+            //ignore token After this, until the end of the line
             parseComment();
-
         }else if(tkz.peek().equals("lw")){
             currentCode = "010";
             tkz.consume();
+            //add regA regB offsetField follow assembly language
             for(int i=0; i<2; i++) {
-//                System.out.println("lw   " + i +tkz.peek());
                 parseReg();
             }
             parseOffsetField();
@@ -77,8 +82,8 @@ public class ParserAssembly {
         }else if(tkz.peek().equals("sw")){
             currentCode = "011";
             tkz.consume();
+            //add regA regB offsetField follow assembly language
             for(int i=0; i<2; i++) {
-//                System.out.println("sw   " + i +tkz.peek());
                 parseReg();
             }
             parseOffsetField();
@@ -86,8 +91,8 @@ public class ParserAssembly {
         }else if(tkz.peek().equals("beq")){
             currentCode = "100";
             tkz.consume();
+            //add regA regB offsetField follow assembly language
             for(int i=0; i<2; i++) {
-//                System.out.println("beq   " + i +tkz.peek());
                 parseReg();
             }
             parseOffsetField();
@@ -95,47 +100,49 @@ public class ParserAssembly {
         }else if(tkz.peek().equals("jalr")){
             currentCode = "101";
             tkz.consume();
+            //add regA regB follow assembly language
             for(int i=0; i<2; i++){
-//                System.out.println("jalr   " + i +tkz.peek());
                 parseReg();
             }
             parseComment();
+            //add bit 0-15 with 0
             parseGapJType();
         }else if(tkz.peek().equals("halt")){
             currentCode = "110";
             tkz.consume();
-//            System.out.println("halt      " + tkz.peek());
             parseComment();
+            //add bit 0-21 with 0
             parseGapOType();
         }else if(tkz.peek().equals("noop")){
             currentCode = "111";
             tkz.consume();
-//            System.out.println("noop      " + tkz.peek());
             parseComment();
+            //add bit 0-21 with 0
             parseGapOType();
-//            System.out.println("after noop   " + tkz.peek());
         }
     }
 
     public void parseLabel() throws Exception{
-//        System.out.println("is this label" + tkz.peek());
+        // check label duplicate
         if(!label.contains(tkz.peek())){
+            //add to label for remember declare label
             label.add(tkz.peek());
+            //add to address for remember address in this code
             address.add(machineCode.size());
-//            System.out.println("label     " + label.getLast() + "address  " + address.getLast());
             tkz.consume();
-//            System.out.println(tkz.peek());
             if(tkz.peek().equals(".fill")){
+                //command fill value in label
                 tkz.consume();
-//                System.out.println("filllllllllllllll "+tkz.peek());
                 parseFill();
             }else if(AssemblyCommand.contains(tkz.peek())){
+                //if next token is command go to read command
                 parseCommand();
             }else{
+                //when syntax is error use command that not in syntax
                 throw new Exception("command not found");
             }
         }else{
-//            System.out.println("why"+tkz.peek());
+            //declare repeat label
             throw new Exception("repeat label");
         }
     }
@@ -144,21 +151,21 @@ public class ParserAssembly {
         if(isNumber(tkz.peek())) {
             int num = Integer.parseInt(tkz.peek());
             if (num <= 32767 && num >= -32768) {
+                //change value from decimal to binary
                 String value = TwosComplement(num);
+                //add it in code
                 currentCode = currentCode + value;
-//                System.out.println("is number fill   " + currentCode);
                 machineCode.add(currentCode);
                 currentCode = "";
-//                System.out.println("fill number?  " +tkz.peek());
                 tkz.consume();
                 parseComment();
             }else{
+                //check if number is over 15 bit
                 throw new Exception("number not match");
             }
         } else {
-//            System.out.println("fill label whit label " + tkz.peek());
+            //add label to code
             currentCode = currentCode + tkz.consume();
-//            System.out.println("what add  " + currentCode);
             machineCode.add(currentCode);
             currentCode = "";
             parseComment();
@@ -166,11 +173,15 @@ public class ParserAssembly {
         }
     }
     public void parseReg() throws Exception{
+        //check is it register
         if(isNumber(tkz.peek())){
+            //check register must be positive
             if(tkz.peek().charAt(0) == '-'){
                 throw new Exception("incorrect reg");
             }else {
+                //change register to binary
                 int reg = Integer.parseInt(tkz.peek());
+                //check if register is exist
                 if(reg <= 7){
                     String r = FillBinary(reg);
                     currentCode = currentCode + r;
@@ -179,71 +190,65 @@ public class ParserAssembly {
                     throw new Exception("wrong register");
                 }
             }
+        }else{
+            throw new Exception("register must be number");
         }
     }
 
     public void parseOffsetField() throws Exception{
-//        System.out.println(isNumber(tkz.peek()));
+        //check offsetField is number or label
         if(isNumber(tkz.peek())){
-//            System.out.println("number offset  " + tkz.peek());
             int num = Integer.parseInt(tkz.peek());
+            //check is number over 15 bit
             if(num <= 32767 && num >= -32768){
+                //change number to binary
                 String offset = TwosComplement(num);
+                //add code in machineCode list
                 currentCode = currentCode + offset;
                 machineCode.add(currentCode);
-//                System.out.println("what add offset num" + currentCode);
                 currentCode = "";
                 tkz.consume();
             }else{
-                throw new Exception("can not read number");
+                throw new Exception("can not use this number");
             }
         }else {
-//            System.out.println("label offset   " + tkz.peek());
+            //add label to code and add code
             currentCode = currentCode + tkz.peek();
-//            System.out.println("what add in label offset  " + currentCode);
             machineCode.add(currentCode);
             currentCode = "";
             tkz.consume();
-//            int index = label.indexOf(tkz.peek());
-//            int value = address.get(index);
-//            if (value <= 32767 && value >= -32768) {
-//                String offset = TwosComplement(value);
-//                currentCode = currentCode + offset;
-//            }
         }
     }
 
     public void fillLabel() throws Exception{
-//        System.out.println("helllll"+machineCode.size()+label.size()+address.size());
+        //start at address 0 for fill label with address follow syntax
         int pc = 0;
         while(pc < machineCode.size()){
             int index = 0;
             String code = machineCode.get(pc);
-//            System.out.println("code for fill   "+code);
             while(index < label.size()){
                 String var = label.get(index);
-//                System.out.println("youuusnpaspcoa'"+var+code);
+                //check if this address is for command or for assign in label
                 if(Character.isDigit(code.charAt(0))){
+                    //select specific label "string" that add before
                     String variable = code.substring(9);
-//                    System.out.println("what substring  " + variable + "what check equals " + var);
+                    //check is this line machineCode have label in declare label
                     if(variable.equals(var)){
+                        //replace label with address in from machine code
                         int adLabel = address.get(index);
+                        //if case J type or I type
                         if (code.charAt(0) == '1') {
-//                        System.out.println("you 11111" + var );
                             int offsetNum = adLabel-(pc+1);
-//                        System.out.println("offsetNum   "+offsetNum);
                             String offset = TwosComplement(offsetNum);
-//                        System.out.println("offset   "+offset);
                             machineCode.set(pc,code.replace(var, offset));
                         } else if(code.charAt(0) == '0') {
-//                        System.out.println("you 0000" + var);
                             String offset = TwosComplement(adLabel);
                             machineCode.set(pc,code.replace(var, offset));
                         }
                     }
                 }else{
+                    //case assign label with another label
                     if(code.equals(var)){
-//                        System.out.println("you fill with label " + var);
                         int adLabel = address.get(index);
                         String offset = TwosComplement(adLabel);
                         machineCode.set(pc,code.replace(var, offset));
@@ -253,7 +258,6 @@ public class ParserAssembly {
             }
             code = machineCode.get(pc);
             if(!isNumber(code)){
-//                System.out.println("what code" + code);
                 throw new Exception("use undefine label");
             }
             pc++;
@@ -261,33 +265,31 @@ public class ParserAssembly {
 
     }
     public void parseGapRType() throws Exception{
+        //add 0 for R type
         StringBuilder code = new StringBuilder(currentCode);
         code.insert(9,"0000000000000");
         currentCode = code.toString();
         machineCode.add(currentCode);
-//        System.out.println("what add R type" + currentCode);
         currentCode = "";
     }
 
     public void parseGapJType() throws Exception{
+        //add 0 for J type
         currentCode = currentCode + "0000000000000000";
         machineCode.add(currentCode);
-//        System.out.println("what add J type" + currentCode);
         currentCode = "";
     }
 
     public void parseGapOType() throws Exception{
+        //add 0 for 0 type
         currentCode = currentCode + "0000000000000000000000";
         machineCode.add(currentCode);
 //        System.out.println("what add 0 type" + currentCode);
         currentCode = "";
     }
 
-    public void parseGap31_25()throws Exception{
-
-    }
-
     public void parseComment() throws Exception{
+        //delete token command
         while (!tkz.peek().equals("!")){
             tkz.consume();
         }
@@ -309,6 +311,7 @@ public class ParserAssembly {
         return true;
     }
     private static List<String> CreateAssemblyCommand(){
+        //add list of command that usable
         List<String> list = new ArrayList<>();
         list.add("add");
         list.add("nand");
@@ -322,6 +325,7 @@ public class ParserAssembly {
     }
 
     public String FillBinary(Integer number) throws Exception{
+        //fill binary number with 0 for correct syntax
         StringBuilder binaryString = new StringBuilder(Integer.toBinaryString(number));
         int size = binaryString.length();
         if(size < 3){
@@ -337,6 +341,7 @@ public class ParserAssembly {
     }
 
     public String TwosComplement(Integer number)throws Exception{
+        //function for change decimal to binary
         if(number >= 0){
             StringBuilder binaryString = new StringBuilder(Integer.toBinaryString(number));
             int size = binaryString.length();
@@ -355,6 +360,7 @@ public class ParserAssembly {
         }
     }
     public void print(){
+        //function for print check
         System.out.println(label.size());
         System.out.println(label);
         System.out.println(address.size());
